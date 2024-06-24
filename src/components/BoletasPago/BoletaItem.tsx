043b09ccd62da3538c6447a5724a608edfbf819d
Rodrigoss06 <102317296+Rodrigoss06@users.noreply.github.com>
2024-06-23 23:01:06 -0500

    boleta item

--- src/components/BoletasPago/BoletaItem.tsx
+++ src/components/BoletasPago/BoletaItem.tsx
@@ -13,25 +13,17 @@ function BoletaItem({boleta}:{boleta:TRS_Boleta_Pago}) {
     try {
       const getEmpleadoData =async()=>{
         const empleadoData = await getEmpleado("https://sia-teo-8rns.vercel.app/api",String(boleta.ID_EMPLEADO))
-        console.log(empleadoData.empleado)
         setEmpleado(empleadoData.empleado)
         const empresaData = await getEmpresa("https://sia-teo-8rns.vercel.app/api",String(empleadoData.empleado.ID_EMPRESA))
-        console.log(empresaData.empresa)
         setEmpresa(empresaData.empresa)
         const boletaPagoDetalleData = await getBoletaPagoDetalle("https://sia-teo-8rns.vercel.app/api",String(boleta.ID_BOLETA_PAGO_DETALLES))
-        console.log(boletaPagoDetalleData.boleta_pago)
         const remuneracionesData = await getMaeRemuneraciones("https://sia-teo-8rns.vercel.app/api")
-        console.log(remuneracionesData.remuneraciones)
         const remuneracionesFilter = remuneracionesData.remuneraciones.filter((remuneracion:MAE_Remuneraciones)=>remuneracion.ID_REMUNERACION_TRANSACCIONAL === boletaPagoDetalleData.boleta_pago.ID_REMUNERACION_TRANSACCIONAL)
-        console.log(remuneracionesFilter)
         setRemuneraciones(remuneracionesFilter)
         const descuentosData = await getMaeDescuentos("https://sia-teo-8rns.vercel.app/api")
-        console.log(descuentosData.descuentos)
         const descuentosFilter = descuentosData.descuentos.filter((descuento:MAE_Descuentos)=>descuento.ID_DESCUENTO_TRANSACCIONAL ===boletaPagoDetalleData.boleta_pago.ID_DESCUENTO_TRANSACCIONAL)
-        console.log(descuentosFilter)
         setDescuentos(descuentosFilter)
         const aportacionesData = await getAportacion("https://sia-teo-8rns.vercel.app/api",String(boletaPagoDetalleData.boleta_pago.ID_APORTACIONES_TRANSACCIONAL))
-        console.log(aportacionesData.aportaciones)
         setAportaciones(aportacionesData.aportaciones)
       }
       getEmpleadoData()
@@ -44,7 +36,7 @@ function BoletaItem({boleta}:{boleta:TRS_Boleta_Pago}) {
       <div>
         <h1 className='text-lg font-semibold'>Datos de la empresa</h1>
         <div className='grid grid-rows-[minmax(30px,_1fr),_minmax(_2fr,50px)]  p-1'>
-        <div className='grid row-span-1 grid-cols-[minmax(50px,_2fr),_minmax(70px,_2fr),_repeat(2,_minmax(120px,_3fr))] '>
+        <div className='grid row-span-1 grid-cols-[minmax(50px,_2fr),_minmax(70px,_2fr),_repeat(2,_minmax(120px,_3fr))] bg-[#DAE4F0] '>
             <p className='border border-black'>RUC:</p>
             <p className='border border-black'>Razón Social:</p>
             <p className='border border-black'>Nombre de la empresa:</p>
@@ -61,7 +53,7 @@ function BoletaItem({boleta}:{boleta:TRS_Boleta_Pago}) {
       <div>
         <h1 className='text-lg font-semibold'>Datos del trabajador</h1>
           <div className='grid grid-rows-[minmax(30px,_1fr),_minmax(50px,_2fr)]'>
-            <div className='grid row-span-1 grid-cols-[minmax(30px,_1fr),_minmax(60px,_2fr),_minmax(30px,_1fr),_repeat(2,_minmax(60px,_2fr)),_minmax(30px,_1fr)]'>
+            <div className='grid row-span-1 grid-cols-[minmax(30px,_1fr),_minmax(60px,_2fr),_minmax(30px,_1fr),_repeat(2,_minmax(60px,_2fr)),_minmax(30px,_1fr)] bg-[#DAE4F0]'>
               <p className='border border-black'>Código:</p>
               <p className='border border-black'>Apellidos y nombres:</p>
               <p className='border border-black'>Documento:</p>
@@ -83,7 +75,7 @@ function BoletaItem({boleta}:{boleta:TRS_Boleta_Pago}) {
         <h1 className='text-lg font-semibold'>Datos del trabajador vinculados a el horario laboral</h1>
         <div>
           <div className='grid grid-rows-[minmax(30px,_1fr),_minmax(50px,_2fr)]'>
-            <div className='grid grid-cols-[minmax(80px,_3fr),_minmax(30px,_1fr),_minmax(50px,_2fr),_minmax(30px,_1fr),_minmax(50px,_2fr)]'>
+            <div className='grid grid-cols-[minmax(80px,_3fr),_minmax(30px,_1fr),_minmax(50px,_2fr),_minmax(30px,_1fr),_minmax(50px,_2fr)] bg-[#DAE4F0]'>
               <p className='border border-black'>Fecha de ingreso:</p>
               <p className='border border-black'>Dias laborales:</p>
               <p className='border border-black'>Total de horas laboradas</p>
@@ -100,9 +92,43 @@ function BoletaItem({boleta}:{boleta:TRS_Boleta_Pago}) {
           </div>
         </div>
       </div>
-      <div className='grid grid-rows-[minmax(30px,_1fr),_minmax(100px,_10fr)] grid-cols-[repeat(_minmax(200px,_2fr))]'>
-        <div className='row-span-2 col-span-1'>
-        
+      <div className='grid grid-rows-[minmax(20px,_3fr),_minmax(100px,_10fr),_minmax(100px,_10fr),minmax(20px,_3fr)] grid-cols-[minmax(20px,_3fr),_minmax(200px,_10fr),_minmax(100px,_6fr),_minmax(100px,_6fr),minmax(20px,_3fr)] border border-black'>
+        <div className='bg-[#DAE4F0] row-span-1 col-span-5 grid grid-cols-[minmax(20px,_3fr),_minmax(200px,_10fr),_minmax(100px,_6fr),_minmax(100px,_6fr),minmax(20px,_3fr)]'>
+          <p className='border border-black'>Código</p>
+          <p className='border border-black'>Descripción</p>
+          <p className='border border-black'>Ingresos S/.</p>
+          <p className='border border-black'>Descuentos S/.</p>
+          <p className='border border-black'>Neto S/.</p>
+        </div>
+        <div className='row-span-1 col-span-5 border border-black'>
+          <h3>Remuneraciones</h3>
+          {remuneraciones?.map((maeRemuneracion)=>(
+            <div key={maeRemuneracion.ID_REMUNERACIONES} className='grid grid-cols-[minmax(20px,_3fr),_minmax(200px,_10fr),_minmax(100px,_6fr),_minmax(100px,_6fr),minmax(20px,_3fr)]'>
+              <p>{maeRemuneracion.ID_REMUNERACIONES}</p>
+              <p>{maeRemuneracion.DESCRIPCION}</p>
+              <p>{maeRemuneracion.MONTO}</p>
+              <p></p>
+              <p></p>
+            </div>
+          ))}
+        </div>
+        <div className='row-span-1 col-span-5 border border-black'>
+          <h3>Descuentos</h3>
+          {descuentos?.map((maeDescuentos)=>(
+            <div key={maeDescuentos.ID_DESCUENTO} className='grid grid-cols-[minmax(20px,_3fr),_minmax(200px,_10fr),_minmax(100px,_6fr),_minmax(100px,_6fr),minmax(20px,_3fr)]'>
+              <p>{maeDescuentos.ID_DESCUENTO}</p>
+              <p>{maeDescuentos.DESCRIPCION}</p>
+              <p></p>
+              <p>{maeDescuentos.MONTO}</p>
+              <p></p>
+            </div>
+          ))}
+        </div>
+        <div className='row-span-1 col-span-5 border border-black'>
+            <div className='grid grid-cols-[minmax(20px,_3fr),_minmax(200px,_10fr),_minmax(100px,_6fr),_minmax(100px,_6fr),minmax(20px,_3fr)]'>
+              <p className='col-span-4'>Neto a Pagar</p>
+              <p>{boleta.NETO_PAGAR}</p>
+            </div>
         </div>
       </div>
     </div>
